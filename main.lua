9669955ac3f9873add718cfbc3bca30a82cb742d
BedrockT <93689518+BedrockT@users.noreply.github.com>
2022-01-02 00:28:12 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -2,6 +2,25 @@
 local neuralI = peripheral.find("neuralI")
 
 --Functions
+function loadModules()
+
+	local readAll = function(tabl)
+		for _, v in pairs(tabl) do
+			if (type(v) == "table") then
+				readAll(v)
+			elseif (type(v) == "string") then
+				os.loadAPI(v);
+			end
+		end
+	end
+	
+	local ModF = io.open("./Modules.txt", "r");
+	local Mods = textutils.unserialise(ModF:read("*a"));
+	
+	ModF:close()
+	
+	
+
 function run()
 
 --Main
