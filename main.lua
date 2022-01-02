653af646412e9fbc49a062cf2f6a0534a6bf4a70
BedrockT <93689518+BedrockT@users.noreply.github.com>
2022-01-02 00:11:44 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,14 @@
+--Variables
+local neuralI = peripheral.find("neuralI")
 
+--Functions
+function run()
+
+--Main
+if (not neuralI.hasModule("plethora:chat")) then
+	print("WARN: Chat recorder (plethora:chat) is recommended");
+end
+
+
+
+while (true) do
