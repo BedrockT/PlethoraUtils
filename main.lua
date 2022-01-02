d0ec18a1ef9b2aa30ba1df24590a304ae2b06ca5
BedrockT <93689518+BedrockT@users.noreply.github.com>
2022-01-02 12:33:05 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,22 +1,24 @@
 --Variables
 local neuralI = peripheral.find("neuralI")
-
+local coroutines = {}
 
 
 --Functions
 function load_file(file)
 	
-	if (file ~= nil) then
+	if (file ~= nil and coroutines[file] == nil) then
 	
-		local ModF = io.open("", "r");
-		local Mods = textutils.unserialise(ModF:read("*a"));
+		local ModF = io.open(filePaths[file], "r");
+		local Mod = loadstring(ModF:read("*a"));
 
-		ModF:close()
+		ModF:close();
+		
+		coroutines[file] = coroutine.create(Mod);
+		
+	end
+end
 	
-	
-
-function abcdef()
-
+		
 --Main
 if (not neuralI.hasModule("plethora:chat")) then
 	print("WARN: Chat recorder (plethora:chat) is recommended");
@@ -25,5 +27,3 @@ end
 local temp = io.open("filePaths.txt", "r");
 filePaths = textutils.unserialise(temp:read("*a"));
 temp:close();
-
-while (true) do
